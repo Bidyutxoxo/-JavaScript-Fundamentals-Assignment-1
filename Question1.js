ede100cdd748c9152bf06d11453633dabd928647
BIDYUT SAIKIA <74451073+Bidyutxoxo@users.noreply.github.com>
2025-01-25 11:41:23 +0530

    Delete Question1.js

--- Question1.js
+++
@@ -1,6 +0,0 @@
-let totalValue = 2000; 
-let discountPercentage = 20; 
-let discountAmount = (totalValue * discountPercentage) / 100;
-let finalPrice = totalValue - discountAmount;
-console.log(`The final price after a ${discountPercentage}% discount is: ₹${finalPrice}`);
-ṇ
