7d86b80210e7bcfe884ae3900f139b9eb2a9de3e
BIDYUT SAIKIA <74451073+Bidyutxoxo@users.noreply.github.com>
2025-01-25 11:45:34 +0530

    Add files via upload

---
+++ PW JAVASCRIPT ASSIGNMENT/Question1.js
@@ -0,0 +1,6 @@
+let totalValue = 2000; 
+let discountPercentage = 20; 
+let discountAmount = (totalValue * discountPercentage) / 100;
+let finalPrice = totalValue - discountAmount;
+console.log(`The final price after a ${discountPercentage}% discount is: ₹${finalPrice}`);
+ṇ
